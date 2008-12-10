0f294539225cd9e910521f7cad2b8e8da86b095a
ivory1988@web.de <ivory1988@web.de@6a76c226-c4d3-11dd-aeb2-5dafaa25f530>
2008-12-10 11:45:10 +0000

    Removed file/folder

--- trunk/mangos to summitemu/gameobject spawns parse.sql
+++
@@ -1,7 +0,0 @@
-delete from gameobject where id in (select Entry from gameobject_spawns);
-
-REPLACE gameobject_spawns (Entry, map, position_x, position_y, position_z, orientation1)
-SELECT id, map, postion_x, position_y, position_z, orientation
-FROM gameobject;
-
-UPDATE gameobject_spawns SET scale = '1' WHERE scale = '0';
