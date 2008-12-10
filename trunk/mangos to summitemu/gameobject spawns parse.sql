3ca951170050fc946ae048caa4ba768443fffd8f
ivory1988@web.de <ivory1988@web.de@6a76c226-c4d3-11dd-aeb2-5dafaa25f530>
2008-12-10 11:46:03 +0000

    Added a file remotely

---
+++ trunk/mangos to summitemu/gameobject spawns parse.sql
@@ -0,0 +1,7 @@
+delete from gameobject where id in (select Entry from gameobject_spawns);
+
+REPLACE gameobject_spawns (Entry, map, position_x, position_y, position_z, orientation1)
+SELECT id, map, position_x, position_y, position_z, orientation
+FROM gameobject;
+
+UPDATE gameobject_spawns SET scale = '1' WHERE scale = '0';
