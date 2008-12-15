7f0548b961cd35878d640cee57a843dda9b0afbf
ivory1988@web.de <ivory1988@web.de@6a76c226-c4d3-11dd-aeb2-5dafaa25f530>
2008-12-15 02:59:42 +0000

    eine letze datei muss noch sein viel spaß ohne mich

---
+++ trunk/updates/alle chars in hyjal.sql
@@ -0,0 +1,11 @@
+update `characters` set `positionZ` = '0';
+update `characters` set `positionY` = '0';
+update `characters` set `positionX` = '0';
+update `characters` set `mapId` = '0';
+update `characters` set `zoneId` = '0';
+
+update `characters` set `positionZ` = '943,9639';
+update `characters` set `positionY` = '-3845,2391';
+update `characters` set `positionX` = '4620,8715';
+update `characters` set `mapId` = '1';
+update `characters` set `zoneId` = '148';
