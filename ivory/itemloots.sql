6eb9fcccf5c1da5049731f8a99e1c64d69b62281
ivory1988@web.de <ivory1988@web.de@6a76c226-c4d3-11dd-aeb2-5dafaa25f530>
2009-01-08 18:21:15 +0000

    so das ist meine arbeit bis jetzt vom neuen changeset^^

---
+++ ivory/itemloots.sql
@@ -0,0 +1,6 @@
+delete from creatureloot where itemid=37700;
+delete from creatureloot where itemid=37703;
+insert into creatureloot (entryid, itemid, percentchance, mincount, maxcount) values 
+(3075, 37700, 100, 1, 10),(27650, 37700, 30, 1, 10),(28547, 37700, 29, 1, 10),(28784, 37700, 28, 1, 10),(30848, 37700, 27, 1, 10),(28546, 37700, 26, 1, 10),(29624, 37700, 25, 1, 10),(30184, 37700, 25, 1, 10),(28858, 37700, 25, 1, 10),(26407, 37700, 19, 1, 10),(24229, 37700, 18, 1, 10),(24601, 37700, 18, 1, 10),(25415, 37700, 18, 1, 1),(32487, 37700, 2, 1, 1);
+insert into creatureloot (entryid, itemid, percentchance, mincount, maxcount) values 
+(30872, 37703, 100, 1, 10),(31813, 37703, 40, 1, 10),(28218, 37703, 33, 1, 10),(30842, 37703, 26, 1, 10),(27382, 37703, 17, 1, 10);
